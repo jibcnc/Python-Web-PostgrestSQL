1e831f417be1c604531135fff0ae834482823506
jibcnc <rivalzjib@gmail.com>
2025-11-19 21:47:08 +0700

    ip,port fix

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def movie_list():
     contacts = my_select()                 
     return render_template("movie.html", contacts=contacts)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=80, debug=True)
